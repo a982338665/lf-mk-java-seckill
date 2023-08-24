34a8ec0be9f92cdae8cd3c6b330fd03dfc876ad9
luofeng <you@example.com>
2023-08-24 22:54:07 +0800

    主题模式-包含广播+直接

---
+++ rabbitmq-hello/src/main/java/pers/li/$7topic/ReceiveLogsTopic02.java
@@ -0,0 +1,32 @@
+package pers.li.$7topic;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import pers.li.utils.RabbitMqUtils;
+
+import java.io.IOException;
+
+public class ReceiveLogsTopic02 {
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] argv) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        //声明 Q2 队列与绑定关系
+        String queueName = "Q2";
+        channel.queueDeclare(queueName, false, false, false, null);
+        channel.queueBind(queueName, EXCHANGE_NAME, "*.*.rabbit");
+        channel.queueBind(queueName, EXCHANGE_NAME, "lazy.#");
+        serr(channel, queueName);
+    }
+
+    static void serr(Channel channel, String queueName) throws IOException {
+        System.out.println("等待接收消息.....");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" 接收队列 :" + queueName + " 绑 定 键:"+delivery.getEnvelope().getRoutingKey()+", 消息:"+message);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
