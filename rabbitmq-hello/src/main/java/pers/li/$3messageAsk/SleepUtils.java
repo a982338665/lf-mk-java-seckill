1a15be1f30196efb28d6bf119b9624476f760e3c
luofeng <you@example.com>
2023-08-24 22:23:28 +0800

    fanout-广播模式

--- rabbitmq-hello/src/main/java/pers/li/$3messageAsk/SleepUtils.java
+++ rabbitmq-hello/src/main/java/pers/li/$3messageAsk/SleepUtils.java
@@ -1,11 +1,11 @@
 package pers.li.$3messageAsk;
 
 public class SleepUtils {
- public static void sleep(int second){
- try {
- Thread.sleep(1000*second);
- } catch (InterruptedException _ignored) {
- Thread.currentThread().interrupt();
- }
- }
+    public static void sleep(int second) {
+        try {
+            Thread.sleep(1000 * second);
+        } catch (InterruptedException _ignored) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
