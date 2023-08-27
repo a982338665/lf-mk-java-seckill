2afd0c2a180b9428ead7764520edd220cf8ba3e8
luofeng <you@example.com>
2023-08-27 21:02:41 +0800

    消息队列填满，无法再次添加消息，也会进入死信队列

---
+++ rabbitmq-hello/src/main/java/pers/li/$8sixin/maxlength/Consumer01.java
@@ -0,0 +1,49 @@
+package pers.li.$8sixin.maxlength;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import pers.li.utils.RabbitMqUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Consumer01 {
+    //普通交换机名称
+    private static final String NORMAL_EXCHANGE = "normal_exchange";
+    //死信交换机名称
+    private static final String DEAD_EXCHANGE = "dead_exchange";
+
+    public static void main(String[] argv) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        //声明死信和普通交换机 类型为 direct
+        channel.exchangeDeclare(NORMAL_EXCHANGE, BuiltinExchangeType.DIRECT);
+        channel.exchangeDeclare(DEAD_EXCHANGE, BuiltinExchangeType.DIRECT);
+        //声明死信队列
+        String deadQueue = "dead-queue";
+        channel.queueDeclare(deadQueue, false, false, false, null);
+        //死信队列绑定死信交换机与 routingkey
+        channel.queueBind(deadQueue, DEAD_EXCHANGE, "lisi");
+        //正常队列绑定死信队列信息
+        Map<String, Object> params = new HashMap<>();
+        //正常队列设置死信交换机 参数 key 是固定值
+        params.put("x-dead-letter-exchange", DEAD_EXCHANGE);
+        //正常队列设置死信 routing-key 参数 key 是固定值
+        params.put("x-dead-letter-routing-key", "lisi");
+//        过期时间，也可以在此处设置，也可以在生产者那里设置，一般会在生产者那里设置，因为可以随时更改，此处表示设置10s
+//        params.put("x-message-ttl",100000);
+//        添加最大长度
+        params.put("x-max-length", 6);
+
+        String normalQueue = "normal-queue";
+        channel.queueDeclare(normalQueue, false, false, false, params);
+        channel.queueBind(normalQueue, NORMAL_EXCHANGE, "zhangsan");
+        System.out.println("等待接收消息.....");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("Consumer01 接收到消息" + message);
+        };
+        channel.basicConsume(normalQueue, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
